4cce71e5ae2fa4633721f8201cdbf9e49a579203
n-batrakov <n-batrakov@users.noreply.github.com>
2020-02-05 15:09:33 +0300

    Fix atob & btoa polyfills

--- packages/cli/src/@internal/polyfills.ts
+++ packages/cli/src/@internal/polyfills.ts
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 const { XMLHttpRequest } = require('xhr2')
 
-const atob = (str: string) => Buffer.from(str, 'base64').toString()
-const btoa = (str: string) => Buffer.from(str).toString('base64')
+const atob = (str: string) => Buffer.from(str, 'base64').toString('binary')
+const btoa = (str: string) => Buffer.from(str, 'binary').toString('base64')
 
 export function polyfill(global: any) {
     global.atob = atob
