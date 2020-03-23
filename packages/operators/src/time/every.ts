f32571bc64c43a44d46f97bc53c6ec8a76b479dd
n-batrakov <n-batrakov@users.noreply.github.com>
2020-03-23 14:08:28 +0300

    Add time helper functions

---
+++ packages/operators/src/time/every.ts
@@ -0,0 +1,11 @@
+import { Observable, interval } from 'rxjs'
+import { mergeMap } from 'rxjs/operators'
+const parseDuration = require('parse-duration')
+
+export function every(opts: string | number) {
+    const ms = typeof opts === 'number' ? opts : parseDuration(opts)
+
+    return (stream: Observable<void>) => stream.pipe(
+        mergeMap(() => interval(ms)),
+    )
+}
