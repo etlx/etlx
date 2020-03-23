feaa75f64fa093dd4f40cd71aec7cd104c752aaf
n-batrakov <n-batrakov@users.noreply.github.com>
2020-03-23 14:07:13 +0300

    Add fetch testing utils

---
+++ packages/operators/src/@internal/testing/fetch.ts
@@ -0,0 +1,21 @@
+jest.mock('node-fetch')
+import fetch from 'node-fetch'
+
+;((global: any) => {
+    const { Response, Headers } = jest.requireActual('node-fetch')
+
+    global.Response = Response
+    global.Headers = Headers
+    global.fetch = fetch
+})(global)
+
+export const jsonResponse = (body: any) => Promise.resolve<Response>(new Response(JSON.stringify(body)))
+export const returnOnce = <TOut>(x: TOut) => (mock: jest.Mock<TOut>) => mock.mockReturnValueOnce(x)
+
+export type MockFetch = jest.Mock<Promise<Response>, [RequestInfo, RequestInit | undefined]>
+export function mockFetch(f?: (x: MockFetch) => MockFetch): MockFetch {
+    let mock: MockFetch = fetch as any
+    mock.mockClear()
+
+    return f === undefined ? mock : f(mock)
+}
