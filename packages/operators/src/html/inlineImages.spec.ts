613e790d582320e09dcde5f7874e951a54189def
n-batrakov <n-batrakov@users.noreply.github.com>
2020-03-23 16:29:03 +0300

    Extract http errors

---
+++ packages/operators/src/html/inlineImages.spec.ts
@@ -0,0 +1,67 @@
+import { mockFetch, returnOnce } from '../@internal/testing/fetch'
+import { inlineImages } from './inlineImages'
+import { promisifyLast } from '../utils'
+import { JSDOM } from 'jsdom'
+
+const host = 'http://example.com'
+const png = 'image/png'
+
+const sut = promisifyLast(inlineImages)
+const body = (dom: JSDOM) => dom.window.document.body.innerHTML
+const respondWithImage = (contentType: string) => Promise.resolve(
+    new Response('img-bytes', {
+        headers: { 'content-type': contentType }
+    }))
+
+describe('inlineImages', () => {
+    it('inline single image', async () => {
+        mockFetch(returnOnce(respondWithImage(png)))
+
+        let init = '<img src="img.png">'
+
+        let actual = await sut(init, { host }).then(body)
+        let expected = '<img src="data:image/png;base64, aW1nLWJ5dGVz">'
+
+        expect(actual).toEqual(expected)
+    })
+
+    it('inline multiple images', async () => {
+        mockFetch(x => {
+            returnOnce(respondWithImage(png))(x)
+            returnOnce(respondWithImage(png))(x)
+
+            return x
+        })
+
+        let init = '<img src="img.png"><img src="img2.png">'
+
+        let actual = await sut(init, { host }).then(body)
+        let expected = '<img src="data:image/png;base64, aW1nLWJ5dGVz">' +
+                       '<img src="data:image/png;base64, aW1nLWJ5dGVz">'
+
+        expect(actual).toEqual(expected)
+    })
+
+    it('preserve non-images', async () => {
+        let init = '<h1>Title</h1><p class="text">Text</p>'
+
+        let actual = await sut(init, { host }).then(body)
+        let expected = init
+
+        expect(actual).toEqual(expected)
+    })
+
+    it('throw on invalid images', async () => {
+        mockFetch(returnOnce(respondWithImage('text/plain')))
+
+        let init = '<img src="img.png">'
+
+        let actual = sut(init, { host }).then(body)
+
+        await expect(actual).rejects.toThrow(new Error('Response Content-Type is not found or is not an image'))
+    })
+
+    it('skip invalid images', async () => {
+
+    })
+})
---
+++ packages/operators/src/http/errors.ts
@@ -0,0 +1,17 @@
+export class HttpStatusError extends Error {
+    constructor(response: { status: number, statusText: string }) {
+        super(`Response status code (${response.status}) does not indicate success`)
+    }
+}
+
+export class HttpMediaTypeError extends Error {
+    constructor(actual: string, expected: string) {
+        super(`Response media type is unexpected. Server responded with ${actual}, but ${expected} is expected`)
+    }
+}
+
+export const faultyResponse = (response: { status: number, statusText: string }) =>
+    new HttpStatusError(response)
+
+export const invalidMediaType = (actual: string, expected: string) =>
+    new HttpMediaTypeError(actual, expected)
