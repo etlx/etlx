922938e1c06480c71c479b2e911869ab73aedeaa
n-batrakov <n-batrakov@users.noreply.github.com>
2020-03-31 10:40:56 +0300

    Fix cli polyfills

--- packages/cli/src/@internal/polyfills.ts
+++ packages/cli/src/@internal/polyfills.ts
@@ -3,19 +3,19 @@ const btoa = (str: string) => Buffer.from(str, 'binary').toString('base64')
 
 function polyfillFetch(global: any) {
     const fetch = require('node-fetch')
+    const ac = require('abort-controller')
 
     global.fetch = fetch
     global.Response = fetch.Response
     global.Headers = fetch.Headers
     global.Request = fetch.Request
-
-    require('abort-controller/polyfill')
+    global.AbortController = ac.AbortController
+    global.AbortSignal = ac.AbortSignal
 }
 
 export function polyfill(global: any) {
     global.atob = atob
     global.btoa = btoa
-    global.fetch = fetch
 
     polyfillFetch(global)
 }
