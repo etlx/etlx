1fda40f926ec8e75672b16127d01fbbb62c52ca8
n-batrakov <n-batrakov@users.noreply.github.com>
2020-04-13 11:03:44 +0300

    Take cli root options from package.json

--- packages/cli/src/@internal/operators/command/operators/root.ts
+++ packages/cli/src/@internal/operators/command/operators/root.ts
@@ -1,4 +1,12 @@
 import commander from 'commander'
+import fs from 'fs'
+import { dirname, join } from 'path'
+
+const defaultOptions = {
+    name: 'etlx',
+    description: 'Rx-based ETL tool',
+    version: '0.1.0',
+}
 
 export type RootCommandOptions = {
     name?: string,
@@ -6,7 +14,11 @@ export type RootCommandOptions = {
     version?: string,
 }
 export const rootCommand = (options?: RootCommandOptions) => {
-    let opts = options || {}
+    let opts: RootCommandOptions = {
+        ...defaultOptions,
+        ...packageJson(),
+        ...options,
+    }
 
     return (cli: commander.Command) => cli
         .name(opts.name || 'etlx')
@@ -17,4 +29,40 @@ export const rootCommand = (options?: RootCommandOptions) => {
             cli.outputHelp()
             process.exit(1)
         })
+}
+
+
+function packageJson<T = unknown>() : T | null {
+    if (require?.main?.filename === undefined) {
+        return null
+    }
+
+    let baseDir = dirname(require.main.filename)
+    let filepath = searchFile(baseDir, 'package.json')
+
+    return filepath === null ? null : parseJson(fs.readFileSync(filepath, 'utf-8'))
+}
+
+function searchFile(baseDir: string, filename: string): string | null {
+    let filepath = join(baseDir, filename)
+    let isExists = fs.existsSync(filepath)
+
+    if (isExists) {
+        return filepath
+    }
+
+    let parent = dirname(baseDir)
+    if (parent === baseDir) {
+        return null
+    }
+
+    return searchFile(parent, filename)
+}
+
+function parseJson(str: string) {
+    try {
+        return JSON.parse(str)
+    } catch {
+        return null
+    }
 }
