b3345c0303929e17ff0b9a152eb8dd55e3037ef1
n-batrakov <n-batrakov@users.noreply.github.com>
2020-04-13 16:49:38 +0300

    Add default logging options

--- packages/cli/src/logging/addLogging.ts
+++ packages/cli/src/logging/addLogging.ts
@@ -2,10 +2,12 @@ import { createLogger as createLogger } from './logger'
 import { loggerConfigSchema, LoggerOptions, LoggerConfig } from './types'
 import { configure } from '../configuration'
 
-export const addLogging = (logger: LoggerOptions) => configure(
+const defaultOptions: LoggerOptions = { level: 'info', raw: false }
+
+export const addLogging = (opts?: LoggerOptions) => configure(
     ctx => ({
         ...ctx,
-        objects: ctx.objects.concat({ logger }),
+        objects: ctx.objects.concat({ logger: opts || defaultOptions }),
         schemes: ctx.schemes.concat(loggerConfigSchema),
         overrides: ctx.overrides.concat((config: LoggerConfig) => ({
             logger: createLogger(config.logging || {}),
