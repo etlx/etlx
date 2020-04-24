0259e561ef886ba2f7c61389ec558a433606889e
n-batrakov <n-batrakov@users.noreply.github.com>
2020-04-24 13:34:25 +0300

    Update fs/exists API

--- packages/operators/src/fs/exists.ts
+++ packages/operators/src/fs/exists.ts
@@ -1,20 +1,11 @@
 import fs from 'fs'
 import { Observable } from 'rxjs'
-import { mergeMap } from 'rxjs/operators'
 
-export const exists = (filepath?: string) => ($: Observable<void | string>) => $.pipe(
-    mergeMap(x => new Observable<boolean>((observer) => {
-        let path = typeof x === 'string' ? x : filepath
-        if (typeof path !== 'string') {
-            observer.error(new Error('Invalid path. Path must be supplied either as operator options or as a stream value.'))
-            return
-        }
+export const exists = (filepath: string) => new Observable<boolean>((observer) => {
+    fs.access(filepath, fs.constants.F_OK, (err) => {
+        let exists = err === null
 
-        fs.access(path, fs.constants.F_OK, (err) => {
-            let exists = err === null
-
-            observer.next(exists)
-            observer.complete()
-        })
-    })),
-)
+        observer.next(exists)
+        observer.complete()
+    })
+})
