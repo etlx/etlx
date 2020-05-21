56b82cbb19d6a1379e5a02b4cc41f66bbae41a43
n-batrakov <n-batrakov@users.noreply.github.com>
2020-05-21 18:07:07 +0300

    Make configuration\addObject generic

--- packages/cli/src/configuration/operators/addObject.ts
+++ packages/cli/src/configuration/operators/addObject.ts
@@ -1,6 +1,6 @@
 import { ConfigurationOptions } from '../types'
 
-export const addObject = (config: any) => (opts: ConfigurationOptions) => ({
+export const addObject = <T>(config: T) => (opts: ConfigurationOptions) => ({
   ...opts,
   objects: [...opts.objects, config],
 })
