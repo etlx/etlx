0c0ad727ba59f98d3cd9d9e16a58051658e15ba4
n-batrakov <n-batrakov@users.noreply.github.com>
2020-05-22 14:36:13 +0300

    Avoid polyfills overwrite

--- packages/cli/src/polyfills.ts
+++ packages/cli/src/polyfills.ts
@@ -1,25 +1,36 @@
-/* eslint-disable no-param-reassign */
+const fetch = require('node-fetch')
+const ac = require('abort-controller')
+
+const merge = (global: any, patch: any) => {
+  Object.entries(patch).forEach(([key, val]) => {
+    if (!(key in global)) {
+      // eslint-disable-next-line no-param-reassign
+      global[key] = val
+    }
+  })
+}
+
+
+function fetchPolyfill() {
+  return {
+    fetch,
+    Response: fetch.Response,
+    Headers: fetch.Headers,
+    Request: fetch.Request,
+    AbortController: ac.AbortController,
+    AbortSignal: ac.AbortSignal,
+  }
+}
 
 const atob = (str: string) => Buffer.from(str, 'base64').toString('binary')
 const btoa = (str: string) => Buffer.from(str, 'binary').toString('base64')
 
-function polyfillFetch(global: any) {
-  // eslint-disable-next-line global-require
-  let fetch = require('node-fetch')
-  // eslint-disable-next-line global-require
-  let ac = require('abort-controller')
-
-  global.fetch = fetch
-  global.Response = fetch.Response
-  global.Headers = fetch.Headers
-  global.Request = fetch.Request
-  global.AbortController = ac.AbortController
-  global.AbortSignal = ac.AbortSignal
-}
-
 export function polyfill(global: any) {
-  global.atob = atob
-  global.btoa = btoa
+  let patch = {
+    atob,
+    btoa,
+    ...fetchPolyfill(),
+  }
 
-  polyfillFetch(global)
+  merge(global, patch)
 }
