652e0751c4b075d0ec9c9ffa6915b6853adf60e0
n-batrakov <n-batrakov@users.noreply.github.com>
2020-05-29 10:14:50 +0300

    Make fetchHistory generic

--- packages/confluence/src/fetchHistory.ts
+++ packages/confluence/src/fetchHistory.ts
@@ -2,8 +2,18 @@ import { pipe, OperatorFunction, of } from 'rxjs'
 import { mergeMap, filter, distinct } from 'rxjs/operators'
 import { expandWhile } from '@etlx/operators/core'
 import { log } from '@etlx/operators/@internal/log'
-import { ConfluenceConfig, ConfluencePageVersion, ConfluencePageExpandable, ConfluencePage } from './types'
+import { ConfluenceConfig, ConfluencePageVersion, ConfluencePageExpandable } from './types'
 import { getPageById } from './getPageById'
+
+type Page = {
+  id: string,
+  version?: ConfluencePageVersion,
+  history?: {
+    lastUpdated?: ConfluencePageVersion,
+    previousVersion?: ConfluencePageVersion,
+    nextVersion?: ConfluencePageVersion,
+  }
+}
 
 type Period = { from?: Date, to?: Date }
 
@@ -21,7 +31,7 @@ const withinPeriod = ({ from, to }: Period) => ({ when }: ConfluencePageVersion)
   return start && end
 }
 
-const checkPeriod = (page: ConfluencePage, opts: FetchHistoryOptions) => {
+const checkPeriod = (page: Page, opts: FetchHistoryOptions) => {
   let { backward, from, to } = opts
 
   let prev = apply(page.history?.previousVersion?.when, parseDate)
@@ -35,18 +45,18 @@ const checkPeriod = (page: ConfluencePage, opts: FetchHistoryOptions) => {
   return !stop
 }
 
-const checkVersion = (page: ConfluencePage, opts: FetchHistoryOptions) => {
+const checkVersion = (page: Page, opts: FetchHistoryOptions) => {
   let version = page.version!.number
   let lastVersion = page.history!.lastUpdated!.number
 
   return opts.backward ? version - 1 > 1 : version + 1 < lastVersion
 }
 
-const hasNextVersion = (opts: FetchHistoryOptions) => (page: ConfluencePage) =>
+const hasNextVersion = (opts: FetchHistoryOptions) => (page: Page) =>
   (!opts || checkPeriod(page, opts)) && checkVersion(page, opts)
 
 const expand: any[] = ['version', 'history.lastUpdated', 'history.previousVersion', 'history.nextVersion']
-const loadNextVersion = (config: ConfluenceConfig, { backward }: FetchHistoryOptions) => (page: ConfluencePage) => {
+const loadNextVersion = (config: ConfluenceConfig, { backward }: FetchHistoryOptions) => (page: Page) => {
   let pageId = page.id
 
   let lastVersion = page.history!.lastUpdated!.number
@@ -62,7 +72,7 @@ const loadNextVersion = (config: ConfluenceConfig, { backward }: FetchHistoryOpt
   return getPageById(pageId, config, { expand, version })
 }
 
-const toChanges = (backward: boolean) => (page: ConfluencePage) => {
+const toChanges = (backward: boolean) => (page: Page) => {
   let prev = page.history!.previousVersion
   let curr = page.version!
   let next = page.history!.nextVersion
@@ -70,7 +80,7 @@ const toChanges = (backward: boolean) => (page: ConfluencePage) => {
   return backward ? of(next, curr, prev) : of(prev, curr, next)
 }
 
-const loadVersionInfo = (config: ConfluenceConfig) => (page: ConfluencePage) => {
+const loadVersionInfo = (config: ConfluenceConfig) => (page: Page) => {
   let expanded = page.version && page.history && page.history.lastUpdated
   let pageId = page.id
   let version = page.version?.number
@@ -84,10 +94,10 @@ export type FetchHistoryOptions = Period & { backward?: boolean }
 
 export const REQUIRED_EXPAND_FIELDS: ConfluencePageExpandable[] = expand.slice()
 
-export const fetchHistory = (
+export function fetchHistory<T extends Page>(
   config: ConfluenceConfig,
   options?: FetchHistoryOptions,
-): OperatorFunction<ConfluencePage, ConfluencePageVersion> => {
+): OperatorFunction<T, ConfluencePageVersion> {
   let opts = options || {}
   let backward = opts.backward || false
 
